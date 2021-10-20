d22c470e8977c10436c1abfd3140fe6ecf158c6f
Kirill <kaynov.w@gmail.com>
2021-10-20 10:01:11 +0300

    add Complex laboratory

---
+++ Java/Laboratory-Classes/src/ru/unn/Main.java
@@ -0,0 +1,11 @@
+package ru.unn;
+
+public class Main {
+
+    public static void main(String[] args)  {
+        Menu menu = new Menu();
+        while(true) {
+            menu.showMainMenu();
+        }
+    }
+}
